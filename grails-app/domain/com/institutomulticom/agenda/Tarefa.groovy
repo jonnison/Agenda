f9604b09f0d9bd8ff7a1e154e20ff4674d922828
Jonnison Lima Ferreira <jonnison.1234@gmail.com>
2011-09-27 11:00:08 -0300

    todos os atributos da classe tarefas e controle de usuario

--- grails-app/domain/com/institutomulticom/agenda/Tarefa.groovy
+++ grails-app/domain/com/institutomulticom/agenda/Tarefa.groovy
@@ -1,8 +1,17 @@
 package com.institutomulticom.agenda
 
 class Tarefa {
-	String descricao
+	
+    String descricao
+    String prioridade
+    Date data    
+    Usuario usuario
+    boolean status
+    Date criado
 
     static constraints = {
+	descricao(nullable:false,blank:false)
+	prioridade(inList:["Informação","Baixa","Media","Alta","Urgente"])
+	data(nullble:true)	
     }
 }
